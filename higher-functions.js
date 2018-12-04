446ae12ec6ac7457db8f84084477d488b627d3ec
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-04 15:29:24 -0500

    warnings

--- higher-functions.js
+++ higher-functions.js
@@ -41,5 +41,34 @@ const filteredNames = filter(myNames, name => name[0] === 'R');
     
 
 
-console.log(filteredNames) // => ['Rich', 'Ray']
+// console.log(filteredNames) // => ['Rich', 'Ray']
 
+
+function hazardWarningCreator(typeOfWarning){
+    let warningCounter = 0;
+    return function(location){
+        warningCounter = warningCounter +1;
+        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}! `)
+        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`)
+    }
+        
+    }
+    
+
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+
+
+rocksWarning('Main St and Pacific Ave');
+// => DANGER There is a Rocks on the Road hazard at Main St and Pacific Ave
+// => The Rocks on the Road hazard has triggered 1 time(s) today!
+
+rocksWarning('Centinela Ave and Olympic Blvd');
+// => DANGER There is a Rocks on the Road hazard at Centinela Ave and Olympic Blvd
+// => The Rocks on the Road hazard has triggered 2 time(s) today!
+
+// Invoke the other hazard creators you instantiated, too
+
+rocksWarning('Main St and Pacifi street');
+
+rocksWarning('Main St and Pacific lane');
